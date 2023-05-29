bcfdfc9a0d83274a48ff0fd353adbe39f632c7f2
chaunghia1992 <nchau0992@gmail.com>
2023-05-29 14:12:20 +0700

    quanlysinhvien.sql

---
+++ Class.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Mark
+(
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
